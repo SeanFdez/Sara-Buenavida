2b515786b2796ca1e4264e186e922561cb616a0b
SeanFdez <danielfdez82@gmail.com>
2024-12-19 14:03:09 +0100

    Lightbox
    Añadido lightbox

--- js/galeria.js
+++ js/galeria.js
@@ -6,3 +6,18 @@ console.log (menu)
 boton.addEventListener(`click`, ()=>{
     menu.classList.toggle(`Activo`)
 })
+
+const imagen=document.querySelectorAll(`.Fotos-img`)
+const lightbox=document.querySelector(`.Lightbox`)
+const cerrar=lightbox.querySelector(`.Lightbox-button`)
+const grande=lightbox.querySelector(`.Lightbox-img`)
+
+imagen.forEach((eachimg, i)=>{
+    imagen[i].addEventListener(`click`,()=>{
+        lightbox.classList.add(`Active`)
+        grande.src=imagen[i].src
+    })
+})
+cerrar.addEventListener(`click`, ()=>{
+    lightbox.classList.remove(`Active`)
+})
