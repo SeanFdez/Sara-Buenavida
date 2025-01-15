73729aa2c0feea331eb224297092749a6a976b94
SeanFdez <danielfdez82@gmail.com>
2025-01-15 19:19:52 +0100

    Comentarios
    añadidos comentarios del texto

--- js/cv.js
+++ js/cv.js
@@ -6,17 +6,19 @@ const section            =document.querySelectorAll(`.Main-section`)
  
 
 //Funciones
+
+//Este es el menú
 let menuHandler = ()=>{
       menu.classList.toggle(`Activo`)
 }
-section.forEach((eachSection, i)=>{
-    console.log(eachSection)
-    const titulo             =eachSection.querySelector(`.Main-titulo`)
+
+//Esta es la funcion de las secciones
+section.forEach((eachSection)=>{
+    const titulo             =eachSection.querySelector(`.Main-titulo`) //Creamos los querySelector de cada seccion.
     const wrap               =eachSection.querySelector(`.Main-wrap`)
     const svg                =eachSection.querySelector(`.Main-svg`)
-    console.log(titulo)
-    console.log(wrap)
-    titulo.addEventListener(`click`, ()=>{
+
+    titulo.addEventListener(`click`, ()=>{                              //Al hacer click en cada seccion, se muestra el wrap y el svg de la flecha se da la vuelta
         wrap.classList.toggle(`Activo`)
         if(wrap.classList.contains(`Activo`)){
             svg.style.transform = "rotate(180deg)"
@@ -27,14 +29,6 @@ section.forEach((eachSection, i)=>{
 
 })
 
-// let acordeonHandler = (_,i)=>{
-//       titulo[i].addEventListener(`click`, ()=>{ 
-//             contenido.forEach((eachContenido, i)=>{
-//                 contenido[i].classList.toggle(`Activo`)
-//             })
-//       })
-// }
 
 //Listener
 botonMenu.addEventListener(`click`, menuHandler)
-// titulo.forEach(acordeonHandler)
--- js/galeria.js
+++ js/galeria.js
@@ -7,14 +7,14 @@ const cerrar        =lightbox.querySelector(`.Lightbox-button`)
 const grande        =lightbox.querySelector(`.Lightbox-img`)
 
 //Funciones
-let botonHandler = ()=> menu.classList.toggle(`Activo`)
-let imagenHandler = (eachimg, i)=>{
+let botonHandler = ()=> menu.classList.toggle(`Activo`)         
+let imagenHandler = (eachimg, i)=>{                             //Cada vez que pulsamos una imagen, mostramos el lightbox añadiendo la clase Activo y hacemos que muestre la imagen en grande
     imagen[i].addEventListener(`click`,()=>{
         lightbox.classList.add(`Active`)
         grande.src=imagen[i].src
     })
 }
-let cerrarHandler = ()=> lightbox.classList.remove(`Active`)
+let cerrarHandler = ()=> lightbox.classList.remove(`Active`)        //Cada vez que se pulsa el boton de cerrar, se cierra el lightbox quitando la clase Activo
 
 //Listeners
 boton.addEventListener(`click`, botonHandler)
